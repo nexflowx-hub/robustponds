7fade1e0e27261d12bea595a502f076340ef087b
Robustponds Dev <dev@robustponds.shop>
2026-06-10 01:38:06 +0000

    e0d6469a-8551-4a89-8da0-005cd058531d

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  turbopack: {
-    root: "/home/z/my-project",
-  },
+  
   images: {
     remotePatterns: [
       {
